06c9637f5dab696912f184e1dce233c5e9b3cd96
Daniel Heslop <daniel@heslop.de>
2025-01-17 22:10:57 +0100

    reviewing webui

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= --xformers --medvram --no-half-vae --no-half --force-enable-xformers --update-all-extensions
+echo 'ALL YOUR BASE ARE BELONG TO US'
 call webui.bat
